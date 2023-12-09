0dba60ba7e8fd469999a3a50dd4c57720f8d43f7
fulvius <fulvius@pop-os.localdomain>
2023-12-09 10:08:48 +0100

    obstacle free implementation of rdt

---
+++ include/RDT.hpp
@@ -0,0 +1,181 @@
+// MIT License
+//
+// Copyright (c) 2023 Fulvio Di Luzio (fulvio.diluzio@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//
+// Created by fulvius on 09/12/23.
+//
+
+#ifndef XI_PLANNING_ALGORITHMS_RDT_HPP
+#define XI_PLANNING_ALGORITHMS_RDT_HPP
+
+#include <vector>
+
+#include "utils.hpp"
+#include "Geometry.hpp"
+
+
+template <typename T, size_t dim>
+class RDT {
+    typedef Point<T, dim> PointT;
+    typedef Node<T, dim> NodeT;
+    typedef PointDistribution<T, dim> PointDistributionT;
+    using Metric = T(*)(NodeT*, NodeT*);
+
+public:
+    RDT() = default;
+    ~RDT() = default;
+    explicit RDT(NodeT* start, NodeT* goal);
+    void setDistribution(PointDistributionT point_distribution) {point_distribution_ = point_distribution;}
+    bool findNearestNeighbor(NodeT*& point, NodeT*& neighbor);
+    bool isGoalWithinRange(NodeT*& point) {return metric_(point, goal_) < goal_range_radius_;}
+    bool run();
+    void getNodes(std::vector<NodeT*>& nodes) {nodes = nodes_;}
+    void getGoal(NodeT*& goal) {goal = goal_;}
+
+
+private:
+    void sampleRandomPoint(NodeT*& point);
+    void projectRandomPointTowardsNearest(NodeT*& point, NodeT*& nearest_point) const;
+    void rewiring(NodeT*& point);
+
+    NodeT* start_;
+    NodeT* goal_;
+
+    Metric metric_ {euclideanMetric};
+    PointDistributionT point_distribution_;
+
+    size_t max_iter_ {1000};
+    T goal_range_radius_ {0.5};
+    T neighbour_radius_ {0.3};
+    T rewiring_radius_ {0.5};
+    std::vector<NodeT*> nodes_;
+};
+
+
+// RDT(NodeT* start, Node* goal)
+template <typename T, size_t dim>
+RDT<T, dim>::RDT(NodeT* start, NodeT* goal) {
+    start_ = start;
+    goal_ = goal;
+}
+
+// void sampleRandomPoint(NodeT*& point)
+template <typename T, size_t dim>
+void RDT<T, dim>::sampleRandomPoint(NodeT*& point) {
+    point_distribution_.sample(*point);
+}
+
+// void findNearestNeighbor(NodeT*& point, NodeT*& neighbor)
+template <typename T, size_t dim>
+bool RDT<T, dim>::findNearestNeighbor(NodeT*& point, NodeT*& neighbor) {
+
+    auto it = std::min_element(
+            nodes_.begin(), nodes_.end(),
+            [point, this](auto &left, auto &right) {
+                return metric_(point, left) < metric_(point, right);
+            }
+    );
+
+    if (it != nodes_.end()) {
+        neighbor = *it;
+        return true;
+    }
+
+    return false;
+}
+
+// void projectRandomPointTowardsNearest(NodeT*& point, NodeT*& nearest_point)
+template<typename T, size_t dim>
+void RDT<T, dim>::projectRandomPointTowardsNearest(NodeT*& point, NodeT*& nearest_point) const {
+    T dist = metric_(point, nearest_point);
+    if (dist > neighbour_radius_) {
+        T rho = neighbour_radius_ / dist;
+
+        for (int i=0; i<dim; i++) {
+            point->operator[](i) = (1 - rho) * nearest_point->operator[](i) + rho * point->operator[](i);
+        }
+    }
+}
+
+// void rewiring(NodeT*& point)
+template<typename T, size_t dim>
+void RDT<T, dim>::rewiring(NodeT*& point) {
+    T dist = 0.0;
+
+    // rewire current node within the ball of radius r
+    for (auto it = nodes_.begin(); it != nodes_.end(); ++it) {
+        dist = metric_(point, *it);
+        if (dist < rewiring_radius_) {
+            if (point->cost > (*it)->cost + dist) {
+                point->parent = *it;
+                point->cost = (*it)->cost + dist;
+            }
+        }
+    }
+
+    // rewire the other nodes within the ball of radius r
+    for (auto it = nodes_.begin(); it != nodes_.end(); ++it) {
+        dist = metric_(point, *it);
+        if (dist < rewiring_radius_ && *it != point->parent) {
+            if ((*it)->cost > point->cost + dist) {
+                (*it)->parent = point;
+                (*it)->cost = point->cost + dist;
+            }
+        }
+    }
+}
+
+// bool run()
+template <typename T, size_t dim>
+bool RDT<T, dim>::run() {
+    nodes_.push_back(start_);
+    size_t iter = 0;
+    while (iter < max_iter_) {
+        NodeT* point = new NodeT;
+        sampleRandomPoint(point);
+        NodeT* nearest_neighbor = new NodeT;
+        if (findNearestNeighbor(point, nearest_neighbor)) {
+            projectRandomPointTowardsNearest(point, nearest_neighbor);
+            // update cost
+            point->parent = nearest_neighbor;
+            T cost_metric = metric_(point, nearest_neighbor);
+            point->cost = nearest_neighbor->cost + cost_metric;
+            // rewiring nodes close to point
+            rewiring(point);
+            nodes_.push_back(point);
+
+            if (isGoalWithinRange(point)) {
+                goal_->parent = point;
+                goal_->cost = point->cost + metric_(point, goal_);
+                printf("Goal found in %zu iterations\n", iter);
+                return true;
+            }
+        }
+        iter++;
+    }
+    return false;
+}
+
+
+#endif //XI_PLANNING_ALGORITHMS_RDT_HPP
+
+
